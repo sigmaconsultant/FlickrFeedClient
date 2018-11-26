b5461819fcbd9c90cc2f01b3358f3ee3be226c4e
sigmaconsultant <sigma@email.com>
2018-11-26 21:48:30 +1100

    added feed

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FeedComponent } from './feed/feed.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      FeedComponent
+   ],
+   imports: [
+      BrowserModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
